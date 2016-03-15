bd03b4653b7dd56daeca9035fe2a9bab067eb8b4
Mihkel Selgal <mihkels@users.noreply.github.com>
2016-03-15 11:10:14 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup
 
 setup(
     name='mdx_grid_tables',
-    version='1.1',
+    version='1.2',
     author='Mihkel Selgal',
     author_email='kasvataja122@hot.ee',
     description='Python-Markdown extension to add grid_table support',
     url='http://activearchives.org/',
-    py_modules=['mdx_gird_tables'],
+    py_modules=['mdx_grid_tables'],
     install_requires=['Markdown>=2.0',],
     classifiers=[
         'Development Status :: 4 - Beta',
