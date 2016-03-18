17480d219f825ac9c9e606719c7ad31611881dcc
Mihkel Selgal <mihkels@users.noreply.github.com>
2016-03-18 14:46:51 +0200

    Changed author and homepage URL

--- setup.py
+++ setup.py
@@ -5,10 +5,9 @@ from setuptools import setup
 setup(
     name='mdx_grid_tables',
     version='1.2',
-    author='Mihkel Selgal',
-    author_email='kasvataja122@hot.ee',
+    author='smartboyathome',
     description='Python-Markdown extension to add grid_table support',
-    url='http://activearchives.org/',
+    url='https://github.com/smartboyathome/Markdown-GridTables/',
     py_modules=['mdx_grid_tables'],
     install_requires=['Markdown>=2.0',],
     classifiers=[
